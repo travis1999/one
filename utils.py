f3d846e7459e4bd9f63f81755520a21de56f1f5c
travis1999 <trevoroguna@gmail.com>
2021-01-23 23:01:57 +0300

    Refactored the error_message function.

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 def error_message(token, line, unit, file=None, message="Not defined"):
-    print(f"Error in execution({unit}) \n")
+    print(f"Error in execution({unit})")
     print(f"File '{file}', line {token.line_number}")
-    print(line)
+    
     length = token.end-token.start if token.end-token.start > 0 else 1
-
     error_mark = "^"*length
-    print(f"{error_mark:>{token.start}}")
-
+    line_f = f"{token.line_number}.| "
+    
+    print(f"{line_f}{line}")
+    print(f"{' '*len(line_f)}{error_mark:>{token.start}}")
     print(f"Error: {message}\n")
+    
