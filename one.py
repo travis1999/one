72bbe743aea610d2d6cc88040521fec736d288e0
travis1999 <trevoroguna@gmail.com>
2021-01-23 23:00:19 +0300

    moved all of the evaluation logic to one function.

--- one.py
+++ one.py
@@ -3,55 +3,60 @@ from oneparser import OneParser
 from execute import Execute
 
 
-def main():
-    print("one version 0.0.1a on python 3.8 64 bit")
+lines = []
+tokenizer = Tokenizer()
+parser = OneParser()
+executer = Execute()
 
-    lines = []
-    tokenizer = Tokenizer()
-    parser = OneParser()
-    executer = Execute()
 
+def evaluate(code: str, clear_env=True) -> str:
+    res, content = tokenizer.tokenize(code)
+
+    if res:
+        parser.raw_prog = tokenizer.raw_program
+        result = parser.parse((x for x in content))
+        if result:
+            evaluation = executer.execute(result, tokenizer.raw_program, clear_env)
+            return evaluation
+            
+    else:
+        print("Failed to execute last line")
+        return False
+
+
+def main() -> None:
+    global lines
+    print("one version 0.0.1a on python 3.9.1 64 bit")
+    env = {}
     while True:
         print()
         line = input(">>> ")
 
         try:
-            if line == "prog":
+            if line == ":prog":
                 for line in lines:
                     print(line)
-            elif line in ["quit", "end"]:
+            elif line in [":quit", ":end"]:
                 quit()
 
-            elif line == "test":
-                with open("test.ne", "r") as code:
-                    cont = code.read()
-                    res, content = tokenizer.tokenize(cont)
+            elif line == ":run":
+                while True:
+                    file_name = input("Input file name: ")
 
-                    if res:
-                        # for y in content:
-                        #     print(y)
-                        parser.raw_prog = tokenizer.raw_program
-                        result = parser.parse((x for x in content))
-                        if result:
-                            evaluation = executer.execute(result)
-                            
-                    else:
-                        print("Failed to execute last line")
-
+                    if file_name == ":cancel":
+                        break
+                    try:
+                        with open(file_name, "r") as code:
+                            cont = code.read()
+                            print()
+                            evaluate(cont)
+                            break
+                    except FileNotFoundError:
+                        print(f"File {file_name} not found.")
 
             else:
-                res, content = tokenizer.tokenize(line)
-                if res:
-                    for y in content:
-                        print(y)
-                    parser.raw_prog = tokenizer.raw_program
-    
-                    result = parser.parse((x for x in content))
-                   
-
-                else:
-                    print("Failed to execute last line")
-
+                evaluate(line, False)
+           
         except KeyboardInterrupt:
             print("Quiting")
             exit(0)
